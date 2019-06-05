f32558eed88207e5076a9e26d6360320af31b531
Richard Bishop <richard@rubiquity.com>
2019-06-05 16:18:36 -0700

    Upgrade to zig 0.4. Wow!

--- build.zig
+++ build.zig
@@ -2,24 +2,15 @@ const Builder = @import("std").build.Builder;
 
 pub fn build(b: *Builder) void {
     const mode = b.standardReleaseOptions();
-    const exe = b.addCExecutable("main");//, "src/main.zig");
-    b.addCIncludePath("deps/");
+    const exe = b.addExecutable("play", "src/main.zig");
 
+    exe.addCSourceFile("deps/picohttpparser.c", [][]const u8{"-std=c99"});
     exe.setBuildMode(mode);
-    exe.addCompileFlags([][]const u8 {
-        "-std=c99"
-    });
-    exe.addSourceFile("deps/picohttpparser.c");
-    const zig_main = b.addObject("main", "src/main.zig");
-    exe.addObject(zig_main);
-    exe.setVerboseLink(true);
+    exe.addIncludeDir("deps/");
 
-    const run_step = b.step("run", "Run the app");
-    const run_cmd = b.addCommand(".", b.env_map, [][]const u8{exe.getOutputPath()});
-    run_step.dependOn(&run_cmd.step);
-    run_cmd.step.dependOn(&exe.step);
-
-    exe.setOutputPath("./main");
     b.default_step.dependOn(&exe.step);
     b.installArtifact(exe);
+
+    const run = b.step("run", "run the client");
+    run.dependOn(&exe.run().step);
 }
--- src/main.zig
+++ src/main.zig
@@ -7,7 +7,7 @@ const c = @cImport({
 //extern fn phr_parse_request(buf: ?[*]const u8, len: usize, method: ?[*](?[*]const u8), method_len: ?[*]usize, path: ?[*](?[*]const u8), path_len: ?[*]usize, minor_version: ?[*]c_int, headers: ?[*]struct_phr_header, num_headers: ?[*]usize, last_len: usize) c_int;
 
 pub fn main() u8 {
-    var req: ?[*]const u8 = c"GET /posts HTTP/1.1\r\nHost: example.com\r\nAccept: text/html\r\nUser-Agent: zig\r\n\r\n";
+    var req: ?[*]const u8 = c"GET /posts HTTP/1.1\r\nHost: bitsandhops.com\r\nAccept: text/html\r\nUser-Agent: zig\r\n\r\n";
     //var req: ?[*]const u8 = c"GET /hoge HTTP/1.1\r\nHost: example.com\r\nUser-Agent: zig\r\n\r\n";
     var req_len: usize = 0;
 
@@ -26,26 +26,18 @@ pub fn main() u8 {
 
     var minor_version: c_int = 0;
 
-    var parse_ret = c.phr_parse_request(req, req_len,
-                                        @ptrCast(?[*]?[*]u8, &method),
-                                        @ptrCast(?[*]usize, &method_len),
-                                        @ptrCast(?[*]?[*]u8, &path),
-                                        @ptrCast(?[*]usize, &path_len),
-                                        @ptrCast(?[*]c_int, &minor_version),
-                                        @ptrCast(?[*]c.phr_header, &headers),
-                                        @ptrCast(?[*]usize, &num_headers),
-                                        0);
+    var parse_ret = c.phr_parse_request(req, req_len, &method, &method_len, &path, &path_len, &minor_version, @ptrCast([*c]c.phr_header, &headers), &num_headers, 0);
     std.debug.warn("req len: {}\n", req_len);
-    std.debug.warn("return: {}\n", parse_ret);
+    std.debug.warn("parsed bytes: {}\n", parse_ret);
     std.debug.warn("method: {} mlen: {} path: {} path_len {} version: {} headers: {}\n", method.?[0..method_len], method_len, path.?[0..path_len], path_len, minor_version, num_headers);
 
     for (headers[0..num_headers]) |h| {
         _ = printHeader(h);
     }
-    return @intCast(u8, parse_ret);
+
+    return 0;
 }
 
 fn printHeader(h: c.phr_header) void {
-    std.debug.warn("{}: {}\n", h.name.?[0..h.name_len], h.value.?[0..h.value_len]);
+    std.debug.warn("{}: {}\n", h.name[0..h.name_len], h.value[0..h.value_len]);
 }
-
