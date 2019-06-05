158e39b65683afa33d7b160e28afce0b05fe9e3b
Richard Bishop <richard@rubiquity.com>
2019-06-05 16:56:18 -0700

    Remove more optionals

--- src/main.zig
+++ src/main.zig
@@ -7,22 +7,17 @@ const c = @cImport({
 //extern fn phr_parse_request(buf: ?[*]const u8, len: usize, method: ?[*](?[*]const u8), method_len: ?[*]usize, path: ?[*](?[*]const u8), path_len: ?[*]usize, minor_version: ?[*]c_int, headers: ?[*]struct_phr_header, num_headers: ?[*]usize, last_len: usize) c_int;
 
 pub fn main() u8 {
-    var req: ?[*]const u8 = c"GET /posts HTTP/1.1\r\nHost: bitsandhops.com\r\nAccept: text/html\r\nUser-Agent: zig\r\n\r\n";
-    //var req: ?[*]const u8 = c"GET /hoge HTTP/1.1\r\nHost: example.com\r\nUser-Agent: zig\r\n\r\n";
-    var req_len: usize = 0;
+    var req: [*]const u8 = c"GET /posts HTTP/1.1\r\nHost: bitsandhops.com\r\nAccept: text/html\r\nUser-Agent: zig\r\n\r\n";
+    var req_len = mem.len(u8, req);
 
-    if (req) |r| {
-        req_len = mem.len(u8, r);
-    }
+    var method: ?[*]const u8 = undefined;
+    var method_len: usize = undefined;
 
-    var method: ?[*]const u8 = null;
-    var method_len: usize = 0;
-
-    var path: ?[*]const u8 = null;
-    var path_len: usize = 0;
+    var path: ?[*]const u8 = undefined;
+    var path_len: usize = undefined;
 
     var headers: [10]c.phr_header = undefined;
-    var num_headers: usize = 10;
+    var num_headers: usize = undefined;
 
     var minor_version: c_int = 0;
 
@@ -32,7 +27,7 @@ pub fn main() u8 {
     std.debug.warn("method: {} mlen: {} path: {} path_len {} version: {} headers: {}\n", method.?[0..method_len], method_len, path.?[0..path_len], path_len, minor_version, num_headers);
 
     for (headers[0..num_headers]) |h| {
-        _ = printHeader(h);
+        printHeader(h);
     }
 
     return 0;
