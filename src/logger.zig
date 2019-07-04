24964c699f6a59b48dd3f1b6ef687a4f1982a3b0
Richard Bishop <richard@rubiquity.com>
2019-07-04 14:57:53 -0700

    Handle errors in log fn

--- src/logger.zig
+++ src/logger.zig
@@ -1,9 +1,13 @@
 const std = @import("std");
 const io = std.io;
 
-pub fn log(comptime format: []const u8, args: ...) !void {
-    const std_out = try io.getStdOut();
+// Being lazy and letting this no-op if we are unable to get
+// or write to std out
+// TODO: Pass stdout in as an argument and return a struct
+// TODO: Make the logger buffered
+pub fn log(comptime format: []const u8, args: ...) void {
+    const std_out = io.getStdOut() catch |err| return;
     var out = std_out.outStream();
     const stream = &out.stream;
-    try stream.print(format, args);
+    stream.print(format, args) catch |err| return;
 }
--- src/main.zig
+++ src/main.zig
@@ -7,14 +7,14 @@ pub fn main() !void {
     var data: [*]const u8 = c"GET /posts HTTP/1.1\r\nHost: bitsandhops.com\r\nAccept: text/html\r\nUser-Agent: zig\r\n\r\n";
     var req = try parser.parse(data);
 
-    try log("Method: {}\n", req.method);
-    try log("Path: {}\n", req.path);
-    try log("Version: {}\n", req.version.toString());
+    log("Method: {}\n", req.method);
+    log("Path: {}\n", req.path);
+    log("Version: {}\n", req.version.toString());
 
     var iter = req.headers.iterator();
 
     while (iter.next()) |h| {
-        try log("{}: {}\n", h.key, h.value);
+        log("{}: {}\n", h.key, h.value);
     }
-    //try logger.log("Accept: {}\n", req.headers.get("Accept").?.value);
+    //log("Accept: {}\n", req.headers.get("Accept").?.value);
 }
