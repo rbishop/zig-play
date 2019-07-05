64313279db27bc60d5617395bdc143415c9a07bd
Richard Bishop <richard@rubiquity.com>
2019-07-04 21:53:08 -0700

    wip: add socket code

--- src/main.zig
+++ src/main.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 const io = std.io;
 const parser = @import("parser.zig");
 const log = @import("logger.zig").log;
+const Socket = @import("socket.zig").Socket;
 
 pub fn main() !void {
     var data: [*]const u8 = c"GET /posts HTTP/1.1\r\nHost: bitsandhops.com\r\nAccept: text/html\r\nUser-Agent: zig\r\n\r\n";
@@ -17,4 +18,7 @@ pub fn main() !void {
         log("{}: {}\n", h.key, h.value);
     }
     //log("Accept: {}\n", req.headers.get("Accept").?.value);
+
+    var sock = Socket.init("127.0.0.1", 8000);
+    log("fd: {}", sock.fd);
 }
